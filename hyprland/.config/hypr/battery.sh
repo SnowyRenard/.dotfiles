31a9af6710250900c830b9ee0f8f8652822b0cbf
Snowy Renard <snowyrenard@gmail.com>
2024-09-25 17:27:59 +0200

    Added basic power savings

---
+++ hyprland/.config/hypr/battery.sh
@@ -0,0 +1,16 @@
+#bin/sh
+
+while true; do
+  state="$(cat /sys/class/power_supply/BAT*/status)"
+
+  if [ $state == "Discharging" ]; then
+    echo "Battery saver"
+    powerprofilesctl set power-saver
+    hyprctl --batch "keyword decoration:blur:enabled 0; keyword decoration:drop_shadow no"
+  else 
+    echo "Performance"
+    powerprofilesctl set performance
+    hyprctl --batch "keyword decoration:blur:enabled 1; keyword decoration:drop_shadow yes"
+  fi
+  sleep 10
+done
